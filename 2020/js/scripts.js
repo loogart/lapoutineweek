533733f194f8d3d6a77471921c75f7c7408d0a12
Chris Soueidan <chris.soueidan@gmail.com>
2019-10-21 16:46:59 -0400

    Update scripts.js

--- 2020/js/scripts.js
+++ 2020/js/scripts.js
@@ -1,8 +1,8 @@
 AOS.init();
 // load header and footer
 $(function () {
-    $("#enHeader").load("../html/en_header.html");
-    $("#enFooter").load("../html/en_footer.html");
-    $("#frHeader").load("./html/fr_header.html");
-    $("#frFooter").load("./html/fr_footer.html");
+    $("#enHeader").load("./html/en_header.html");
+    $("#enFooter").load("./html/en_footer.html");
+    $("#frHeader").load("../html/fr_header.html");
+    $("#frFooter").load("../html/fr_footer.html");
 });
