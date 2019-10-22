d35ac841c980c0b7c1560c8fd45b8345509408d0
Chris Soueidan <chris.soueidan@gmail.com>
2019-10-22 01:40:14 -0400

    completed LPW pre-festival website

--- 2020/js/scripts.js
+++ 2020/js/scripts.js
@@ -4,7 +4,13 @@ $(function () {
     $("#enHeader").load("./html/en_header.html");
     $("#enFooter").load("./html/en_footer.html");
     $("#enWinners").load("./html/en_winners.html");
-    
+
     $("#frHeader").load("../html/fr_header.html");
     $("#frFooter").load("../html/fr_footer.html");
+});
+
+//registration fee
+
+$('select.region').on('change', function () {
+    $(".price").html(this.value);
 });
