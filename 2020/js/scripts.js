f06fd87bf3e296c072d483a826b6ac68d36cc071
Chris Soueidan <chris@loogart.com>
2020-01-13 00:19:17 -0500

    live app navbar

--- 2020/js/scripts.js
+++ 2020/js/scripts.js
@@ -3,8 +3,9 @@ AOS.init();
 $(function () {
     $("#enHeader").load("./html/en_header.html");
     $("#enFooter").load("./html/en_footer.html");
-//    $("#enWinners").load("./html/en_winners.html");
-
+    $("#enHeaderLive").load("../html/en_header_live.html");
+    $("#enFooterLive").load("../html/en_footer_live.html");
+    //    $("#enWinners").load("./html/en_winners.html");
     $("#frHeader").load("../html/fr_header.html");
     $("#frFooter").load("../html/fr_footer.html");
 });
@@ -13,4 +14,18 @@ $(function () {
 
 $('select.region').on('change', function () {
     $(".price").html(this.value);
+});
+
+$('.switch-1').click(function(){
+    $('.mobile-nav-open').toggleClass("d-none d-block");
+    $('body').toggleClass("scrollable non-scrollable");
+});
+
+$('.switch-2').click(function(){
+    $('.search-mobile').toggleClass("d-none d-block");
+    $("#searchInput").focus();
+});
+$('.switch-3').click(function(){
+    $('.search-desktop').toggleClass("d-none d-block");
+    $("#searchInput2").focus();
 });
